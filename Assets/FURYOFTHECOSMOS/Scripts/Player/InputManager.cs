508a0925ab815e2ecb1336a27967b7a2ff6458d1
Akın <kekikcilerakin@gmail.com>
2024-06-15 21:48:29 +0300

    Imported my own TPS Controller

---
+++ Assets/FURYOFTHECOSMOS/Scripts/Player/InputManager.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Instance { get; private set; }
+        
+    private PlayerInputActions inputActions;
+
+    public Vector2 Move { get; private set; }
+    public Vector2 Look { get; private set; }
+    public bool Run { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+
+        inputActions = new PlayerInputActions();
+
+        inputActions.Player.Move.performed += OnMove;
+        inputActions.Player.Look.performed += OnLook;
+
+        inputActions.Player.Run.performed += OnRun;
+        inputActions.Player.Run.canceled += OnRun;
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        Move = context.ReadValue<Vector2>();
+    }
+
+    private void OnLook(InputAction.CallbackContext context)
+    {
+        Look = context.ReadValue<Vector2>();
+    }
+
+    private void OnRun(InputAction.CallbackContext context)
+    {
+        Run = context.ReadValueAsButton();
+    }
+    
+    public void DisableMovement()
+    {
+        Move = Vector2.zero;
+        Look = Vector2.zero;
+        inputActions.Player.Look.Disable();
+        inputActions.Player.Move.Disable();
+    }
+
+    public void EnableMovement()
+    {
+        inputActions.Player.Look.Enable();
+        inputActions.Player.Move.Enable();
+    }
+}
---
+++ Assets/FURYOFTHECOSMOS/Scripts/Player/PlayerController.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController Instance { get; private set; }
+
+    private CharacterController characterController;
+
+    [SerializeField] private float walkSpeed = 1.8f;
+    [SerializeField] private float runSpeed = 4.5f;
+
+    #region Constant Variables
+    private const float Gravity = -9.81f;
+    private const float SpeedChangeRate = 10.0f;
+    private const float SpeedOffset = 0.1f;
+    private const float InputMagnitude = 1.0f;
+    private const float RotationSmoothness = 0.1f;
+    #endregion
+
+    #region Cached Variables
+    private float speed;
+    private float targetRotation;
+    private float rotationVelocity;
+    #endregion
+
+    private void Awake()
+    {
+        Instance = this;
+
+        characterController = GetComponent<CharacterController>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        HandleMove();
+        HandleRotate();
+    }
+
+    private void HandleMove()
+    {
+        float targetSpeed = InputManager.Instance.Run ? runSpeed : walkSpeed;
+        if (InputManager.Instance.Move == Vector2.zero) targetSpeed = 0.0f;
+
+        AccelerateSpeed(targetSpeed);
+
+        Vector3 targetDirection = Quaternion.Euler(0.0f, targetRotation, 0.0f) * Vector3.forward;
+
+        characterController.Move(targetDirection.normalized * (speed * Time.deltaTime) + new Vector3(0.0f, Gravity, 0.0f) * Time.deltaTime);
+    }
+
+    private void HandleRotate()
+    {
+        if (InputManager.Instance.Move == Vector2.zero) return;
+
+        Vector3 lookDirection = new Vector3(InputManager.Instance.Move.x, 0.0f, InputManager.Instance.Move.y).normalized;
+
+        targetRotation = Mathf.Atan2(lookDirection.x, lookDirection.z) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
+        float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref rotationVelocity, RotationSmoothness);
+
+        transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+    }
+
+    private void AccelerateSpeed(float targetSpeed)
+    {
+        float currentHorizontalSpeed = new Vector3(characterController.velocity.x, 0.0f, characterController.velocity.z).magnitude;
+
+        if (currentHorizontalSpeed < targetSpeed - SpeedOffset || currentHorizontalSpeed > targetSpeed + SpeedOffset)
+        {
+            speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * InputMagnitude, Time.deltaTime * SpeedChangeRate);
+            speed = Mathf.Round(speed * 1000f) / 1000f;
+        }
+        else
+        {
+            speed = targetSpeed;
+        }
+    }
+
+    public float GetMoveAmount()
+    {
+        float horizontalInput = InputManager.Instance.Move.x;
+        float verticalInput = InputManager.Instance.Move.y;
+
+        float moveAmount = Mathf.Clamp01(Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput));
+
+        if (InputManager.Instance.Run) return moveAmount *= 2f;
+        else return moveAmount;
+    }
+}
